2319aaee0656424be459cd1b00b1c84c62e293da
fidanLimani <f.limani@seeu.edu.mk>
2017-11-18 00:56:38 +0100

    JGit classes.

---
+++ src/main/java/jgit/HarvestUtil.java
@@ -0,0 +1,49 @@
+package jgit;
+
+import java.io.File;
+import java.io.IOException;
+
+public class HarvestUtil {
+    public static File createWorkingDir() throws IOException {
+        String path = System.getProperty("user.home") + File.separator + "GitHub" + File.separator;
+        // System.out.println("Path string: " + path);
+        File dir = new File(path);
+        if (dir.exists()) {
+            System.out.println(dir + " already exists");
+            // System.out.println("Canonical path: " + dir.getCanonicalPath());
+            // System.out.println("dir toString(): " + dir.toString());
+            return dir;
+        } else if (dir.mkdirs()) {
+            System.out.println(dir + " was created");
+            return dir;
+        } else {
+            System.out.println(dir + " was not created");
+            return null;
+        }
+    }
+
+    // Does a repo already exist?
+    public static boolean repoExists(String repoName) throws IOException {
+        boolean status = false;
+        String[] fileList = HarvestUtil.createWorkingDir().list();
+
+        for(String str : fileList){
+            if(repoName.equals(str)){
+                System.out.println(repoName + " already exists!");
+                status = true; //return Git reference to it!
+            }
+        }
+
+        return status;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String repoName = "ElasticSearch";
+        File testFile = HarvestUtil.createWorkingDir();
+        System.out.println("File (canonical): " + testFile); // .getCanonicalFile()
+        File localFileRepo = new File(testFile + File.separator + repoName + File.separator + "local");
+        System.out.println("localFileRepo: " + localFileRepo);
+
+        System.out.println("Project exists? " + HarvestUtil.repoExists("ElasticSearch"));
+    }
+}
---
+++ src/main/java/jgit/JGitUtil.java
@@ -0,0 +1,311 @@
+package jgit;
+
+import java.io.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.eclipse.jgit.api.CheckoutCommand; // Required for (a future) checking out certain directories from a Git project.
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.api.ResetCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.merge.MergeStrategy;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.transport.FetchResult;
+import org.eclipse.jgit.transport.TrackingRefUpdate;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
+import org.eclipse.jgit.lib.ProgressMonitor;
+
+/* Notes
+    + 0. Create local repository to hold SOEP dataset; in case there already is one, just get back the reference to it
+    in order to support the next step - repo. cloning.
+    + 1. Clone SOEP locally:
+    + 2. Check whether a repo. with certain name already exists
+    + 3. Fetch changes in remote repo: return a boolean value after comparing local and remote repos.
+    + 4. Update local repo from remote URI
+* */
+
+public class JGitUtil {
+    private File gitDir; // local base GitHub dir
+    private String repoName;
+    private String repoRemoteUri;
+    private String datasetPath;
+    private File localFileRepo;
+    private File remoteFileRepo;
+    private Git remoteGit;
+    private Git localGit;
+    private final String MASTER = "refs/heads/master"; // HEAD of local (cloned) repo
+    private final String ORIGIN_MASTER = "refs/remotes/origin/master"; // HEAD in remote repo
+
+    // Constructor
+    public JGitUtil(String repoName, String repoRemoteUri, String datasetPath) throws IOException {
+        this.gitDir = HarvestUtil.createWorkingDir(); // The base GitHub directory created on [user.home] path
+        this.repoName = repoName;
+        this.datasetPath = datasetPath;
+        this.repoRemoteUri = repoRemoteUri;
+        localFileRepo = new File(gitDir + File.separator + repoName + File.separator + "local");
+        remoteFileRepo = new File(gitDir + File.separator + repoName + File.separator + "remote");
+        remoteGit = localGit = null;
+    }
+
+    // Set up the local repository: initialize and clone, if local repo does not exist
+    public void setUp() throws GitAPIException, IOException {
+        if(repoExists()) {
+            System.out.println("Repo <" + repoName + "> exists.");
+            setLocalGit(); // A repo exists during setUp()
+        } else {
+            remoteGit = initRepo(); // init and clone repo
+            localGit = cloneRepo();
+            // checkOut(); // check out only certain directory (future work);
+        }
+    }
+
+    // 0. Initialize repository: in case it exists, get a reference to it
+    public Git initRepo() throws GitAPIException, IOException {
+        System.out.println("\nInitializing repository <" + repoName + ">");
+        return Git.init().setDirectory(remoteFileRepo).call();
+    }
+
+    // 1. Clone (SOEP) repository
+    public Git cloneRepo() throws GitAPIException, IOException {
+        System.out.println("\nCloning remote repository from <" + repoRemoteUri + ">");
+        return Git.cloneRepository()
+                .setProgressMonitor(new TextProgressMonitor(new PrintWriter(System.out)))
+                .setURI(repoRemoteUri)
+                .setDirectory(localFileRepo)
+                .call();
+    }
+
+    /* Future development
+    // 1.2 Checkout
+    public void checkOut() throws GitAPIException {
+        // repoBranch: ORIGIN_MASTER; ElasticSearch project path: "origin/dataset"
+        System.out.printf("%nCheck out path <%s> on branch <%s>.", datasetPath, ORIGIN_MASTER);
+        getLocalGit().checkout().setStartPoint(ORIGIN_MASTER).addPath(datasetPath).call();
+    }
+    */
+
+    /* 2. Does the repo exists? This method could take a parameter, in case there are more repositories available
+            locally: gitDir field is assigned the root GitHub directory! */
+    public boolean repoExists() {
+        boolean status = false;
+        try {
+            Git tempGit = Git.open(remoteFileRepo);
+            status = true;
+        } catch (RepositoryNotFoundException e){
+            System.err.println("Repository does not exist. To be created next.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return status;
+    }
+
+    // 3. Fetch changes in remote repo
+    public boolean fetchRepo(String repoBranch){
+        boolean status = false;
+        System.out.printf("Repository updates from <%s> available?",  repoBranch);
+        ProgressMonitor monitor = new TextProgressMonitor(new PrintWriter(System.out));
+        try{
+            FetchResult fetch = getLocalGit().fetch().setCheckFetchedObjects(true).setProgressMonitor(monitor).call();
+            TrackingRefUpdate refUpdate = fetch.getTrackingRefUpdate(repoBranch);
+            if(refUpdate != null) {
+                RefUpdate.Result result = refUpdate.getResult();
+                System.out.printf("%n\tUpdates available. Pull changes!"); // We represent this information via the
+                status = true;                                          // return value in <status> of the method.
+            } else {
+                System.out.printf("%n\t(Local) Repository up to date.");
+            }
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+
+        return status;
+    }
+
+    // 4. Update local repo from remote URI
+    public void updateRepo()throws GitAPIException {
+        System.out.println("\nUpdating local repository <" + localFileRepo.getAbsolutePath() + ">");
+        ProgressMonitor monitor = new TextProgressMonitor(new PrintWriter(System.out));
+
+        // checkOut(); // Check out the dataset of interest before pulling resources to local repo. (future work)
+
+        try (Git git = getLocalGit()){
+            PullResult pullRes = git.pull()
+                    .setProgressMonitor(monitor)        // URI: http://download.eclipse.org/jgit/docs/jgit-2.0.0.201206130900-r/apidocs/org/eclipse/jgit/merge/MergeStrategy.html
+                    .setStrategy(MergeStrategy.THEIRS) // Simple strategy that sets the output tree to the second input tree.
+                    .call();
+
+            if(pullRes.isSuccessful()){
+                String head = git.reset()   // field <head> used for testing purposes
+                        .setMode(ResetCommand.ResetType.HARD)
+                        .setRef(ORIGIN_MASTER)
+                        .call()
+                        .getName();
+                System.out.println("%nRepository successfully updated.");
+            }
+        }
+    }
+
+    /*
+        Multiple methods that were used to assess different aspects of Git repositories; they could be used in the
+        future development of the harvester, hence their inclusion at this point.
+    * */
+
+    // Exploring the remote repository
+    public void exploreRepo(String repoBranch) throws IOException {
+        Repository repo = getLocalGit().getRepository();
+        System.out.printf("\nExploring branch: %s%n%n", repoBranch);
+        try(RevWalk revWalk = new RevWalk(repo)){
+            ObjectId commitId = repo.resolve(repoBranch);
+
+            revWalk.markStart(revWalk.parseCommit(commitId));
+            for(RevCommit commit : revWalk){
+                System.out.println("Commit message: " + commit.getFullMessage());
+            }
+        }
+    }
+
+    // Repository information
+    public void repoInfo(){
+        Map<String, Ref> refs = getLocalGit().getRepository().getAllRefs();
+        System.out.printf("%nAll Refs (%d)%n", refs.size());
+
+        Ref head = refs.get(Constants.HEAD);
+        if (head == null) {
+            System.out.println("HEAD ref is dead and/or non-existent?");
+            return;
+        }
+
+        Map<String, Ref> printRefs = new TreeMap();
+        String current = head.getLeaf().getName(); // Start from the HEAD ref of the repository;
+        if (current.equals(Constants.HEAD)) {
+            printRefs.put("(no branch)", head);
+        }
+
+        for (Ref ref : RefComparator.sort(refs.values())) {
+            String name = ref.getName();
+            if (name.startsWith(Constants.R_HEADS) || name.startsWith(Constants.R_REMOTES)) {
+                printRefs.put(name, ref);
+            }
+        }
+
+        int maxLength = 0; // Find the longest key in Map<String, Ref> printRefs; to be used during print out.
+        for (String name : printRefs.keySet()) {
+            maxLength = Math.max(maxLength, name.length());
+        }
+
+        System.out.printf("Refs (Heads/Remotes) (%d)%n", printRefs.size());
+        for (Map.Entry<String, Ref> e : printRefs.entrySet()) {
+            Ref ref = e.getValue();
+            ObjectId objectId = ref.getObjectId();
+            System.out.printf("%c %-" + maxLength + "s %s%n", (current.equals(ref.getName()) ? '*' : ' '), e.getKey(),
+                    objectId.abbreviate(8).name()); // Could be abbreviated to any lenght;
+        }
+    }
+
+    public void listBranches() throws GitAPIException {
+        System.out.println("\nListing remote branches for: " + getLocalGit().getRepository().toString());
+        List<Ref> branches = localGit.branchList().call();
+        for (Ref ref : branches) {
+            System.out.printf("<Branch> %s %n<Branch name> %s %n<Branch Object ID> %s",
+                    ref, ref.getName(), ref.getObjectId().getName());
+        }
+    }
+
+    // Find SOEP dataset from cloned repository: maybe return SHA-1value(s) of the files that comprise the dataset
+    public void idDatasetFiles(Repository repo, String searchTerm) throws IOException {
+        ObjectId lastCommitId = repo.resolve(Constants.HEAD);
+
+        try(RevWalk revWalk = new RevWalk(repo)){
+            RevCommit commit = revWalk.parseCommit(lastCommitId);
+
+            // Use the tree of the latest commit to find files of interest
+            RevTree tree = commit.getTree();
+
+            // Find a specific file
+            try(TreeWalk tWalk = new TreeWalk(repo)){
+                tWalk.addTree(tree);
+                tWalk.setRecursive(true);
+                tWalk.setFilter(PathFilter.create(searchTerm)); // E.g.: datasets/abroad.json
+
+                /*
+                if(!tWalk.next()){
+                    throw new IllegalStateException("Did not find" + searchTerm);
+                }
+                */
+
+                ObjectId objId = tWalk.getObjectId(0);
+                ObjectLoader loader = repo.open(objId);
+                loader.copyTo(System.out);
+            }
+
+            revWalk.dispose();
+        }
+    }
+
+    /*
+        Getters, setters, and utility methods
+    * */
+    public Git getLocalGit() {
+        return localGit;
+    }
+    public Git getRemoteGit() {
+        return remoteGit;
+    }
+    public String getRepoRemoteUri() { return repoRemoteUri; }
+
+    public void setRepoName(String repoName){
+        System.out.println("\nSetting the repository name to: " + repoName);
+        this.repoName = repoName;
+    }
+    public void setRepoRemoteUri(String repoRemoteUri){
+        System.out.println("\nSetting the remote repository URI to: " + repoRemoteUri);
+        this.repoRemoteUri = repoRemoteUri;
+    }
+
+    // If field <localGit> is null, assign it a Git reference;
+    public void setLocalGit(){
+        if(localGit == null){
+            try {
+                localGit = Git.open(localFileRepo);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /*
+        Demo the application
+    */
+    public static void main(String[] args) throws IOException, GitAPIException {
+        // ElasticSearch GitHub project
+        String esRemoteRepo = "https://github.com/fidanLimani/ElasticSearch";
+        String esDatasetPath = "dataset"; // ElasticSearch GitHub path: master/dataset
+
+        // SOEP-core GitHub project
+        String soepRemoteRepo = "https://github.com/paneldata/soep-core";
+        String soepDatasetPath = "ddionrails/datasets"; // SOEP directory of interest
+
+        // Init & clone a repository: ElasticSearch porject
+        // JGitTest gHubES = new JGitTest("ElasticSearch", esRemoteRepo, esDatasetPath);
+        JGitUtil gHubSoep = new JGitUtil("SOEP-core", soepRemoteRepo, soepDatasetPath);
+
+        gHubSoep.setUp();
+
+        if(gHubSoep.fetchRepo(gHubSoep.ORIGIN_MASTER)) { // Update local repo. if required
+            gHubSoep.updateRepo();
+        }
+
+        //gHubES.exploreRepo(gHubES.ORIGIN_MASTER);
+
+        System.out.printf("%n%nApplication completed.");
+    }
+}
