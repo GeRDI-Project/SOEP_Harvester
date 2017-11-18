b5538aa685957e48374719f5de1559b94e61a30b
fidanLimani <f.limani@seeu.edu.mk>
2017-11-19 00:00:16 +0100

    .gitignore update

---
+++ src/main/java/soep/SOEPHarvester.java
@@ -0,0 +1,10 @@
+package soep;
+/*
+    The main harvester
+* */
+public class SOEPHarvester {
+    // As suggested, the constructor should be in a "default" style
+    public SOEPHarvester(){
+
+    }
+}
