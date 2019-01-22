749dc2ff421bd373cf8e2f0de4a7f3bdc15274c1
fidanLimani <fidan.limani2016@gmail.com>
2019-01-22 15:27:51 +0100

    SAI-659: - PMD recommendations addressed.

--- src/main/java/de/gerdiproject/harvest/etls/extractors/SoepExtractor.java
+++ src/main/java/de/gerdiproject/harvest/etls/extractors/SoepExtractor.java
@@ -24,9 +24,6 @@ import java.io.StringReader;
 import java.lang.reflect.Type;
 import java.nio.file.DirectoryStream;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 
 import com.google.gson.reflect.TypeToken;
